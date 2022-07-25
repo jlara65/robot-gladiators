44140d4275af836898d5e493361bb0e10f6da79d
Joseph L <vipercar233@gmail.com>
2022-07-25 02:08:53 -0500

    partly added function, variables, and operators

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,18 +1,36 @@
-//this creates a function named "fights"
-
-function fight() {
-    window.alert("The fight has begun!")
-}
-//fight ();
-
 var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log("This logs a string, good for leaving yourself a message");
+//log mutlipe values at once
+console.log(playerName, playerAttack, playerHealth);
 
-// this will do math and log 20
+var enemyName = "Roberto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-console.log(10 + 10);
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+};
 
-// What is this?
+fight();
 
-console.log(playerName);
+//Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable
+
+enemyHealth = enemyHealth - playerAttack;
+
+//Log a reslulting message to the console so we know that it worked.
+
+console.log(
+playerName + "attacked" + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+);
+//Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable
+
+playerHealth = playerHealth - enemyAttack;
+
+//Log a resluting mesage to the console so we know it worked.
+
+console.log (
+    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+);
+
