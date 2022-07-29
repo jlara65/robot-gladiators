44cf2ffe5e65e6d7e44cf81f9289b0241f43b8ae
Joseph L <vipercar233@gmail.com>
2022-07-28 21:41:12 -0500

    Integer input in shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -145,24 +145,27 @@ var playAgainConfirm = window.confirm("Would you like to play again?");
 //shop between battle function 
 var shop = function() {
     var shopOptionPrompt = window.prompt (
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
     );
+
+    shopOptionPrompt = parseInt(shopOptionPrompt);
 
     // use switch to carry out action 
     switch (shopOptionPrompt) {
-        case "refill":
-        case "REFILL":
+
+        case 1:
+
            playerInfo.refillHealth();
             break;
 
-        case "upgrade":
-        case "UPGRADE":
+        case 2:
            playerInfo.upgradeAttack();
             break;
-        case "leave":
-        case "LEAVE":
-                window.alert("Leaving the store.");
+
+        case 3:
+            window.alert("Leaving the store.");
             break;
+
         default:
             window.alert("you did not pick a valid option. Try again.");
 
